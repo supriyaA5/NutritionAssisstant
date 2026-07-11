ebca72e5cb90cf87d6ec9adb2e24064b314056f1
Supriya Alla <saisupriyaalla982@gmail.com>
2026-07-11 17:53:56 +0530

    Updated server for deployment

--- server/index.js
+++ server/index.js
@@ -27,8 +27,10 @@ app.get("/", (req, res) => {
   res.send("Server running");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
 
 // POST API (SAVE FOOD)
